f1086032755678f21e41b23f31672802ec10d234
MUBEEN280 <mubeenmubeenmubeen770@gmail.com>
2025-04-15 15:31:49 -0700

    Updated Navbar in Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,7 +5,7 @@ import './App.css'; // Your custom styles if any
 function App() {
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>Hello Mubeen</h1>
       <button className="btn btn-primary">Hello Mubeen</button>
     </div>
   );
